3d83b5ef6b4c405ecccb9379a3ae1cf9b6c6c356
zhuzz <13075810607@163.com>
2026-05-28 15:21:35 +0800

    feat(database): Add basic entity classes and dependency configurations

---
+++ spring-whale-database/src/main/java/io/github/springwhale/database/BaseEntity.java
@@ -0,0 +1,45 @@
+package io.github.springwhale.database;
+
+import io.github.springwhale.framework.core.utils.AuthUtil;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@SQLDelete(sql = "UPDATE #{entityName} SET del_flag = 1, update_time = now() WHERE id = ?")
+@SQLRestriction("del_flag = 0")
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private Integer createBy;
+
+    private Integer updateBy;
+    /**
+     * 0 - normal，1 - delete
+     */
+    private Integer delFlag = 0;
+
+    @PrePersist
+    public void prePersist() {
+        this.createTime = LocalDateTime.now();
+        this.updateTime = LocalDateTime.now();
+        this.createBy = AuthUtil.getUserId();
+        this.updateBy = AuthUtil.getUserId();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updateTime = LocalDateTime.now();
+        this.updateBy = AuthUtil.getUserId();
+    }
+}
---
+++ spring-whale-database/src/main/java/io/github/springwhale/database/SimpleBaseEntity.java
@@ -0,0 +1,27 @@
+package io.github.springwhale.database;
+
+import io.github.springwhale.framework.core.utils.AuthUtil;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public abstract class SimpleBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createTime;
+
+    private Integer createBy;
+
+    // 插入时自动赋值
+    @PrePersist
+    public void prePersist() {
+        this.createBy = AuthUtil.getUserId();
+        this.createTime = LocalDateTime.now();
+    }
+}
