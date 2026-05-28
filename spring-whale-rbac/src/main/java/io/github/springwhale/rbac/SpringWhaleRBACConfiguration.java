dd4450f137a36861e17bf91aa6d92515f26b6489
zhuzz <13075810607@163.com>
2026-05-28 14:38:04 +0800

    refactor(rbac): remove unused imports from configuration class
    - Removed SpringWhaleWebMvcResponseBodyAdvice import
    - Removed SpringWhaleWebMvcExceptionHandler import
    - Removed SpringWhaleWebMvcExceptionProperties import
    - Removed unused Import annotation
    - Cleaned up unnecessary framework imports in RBAC configuration

--- spring-whale-rbac/src/main/java/io/github/springwhale/rbac/SpringWhaleRBACConfiguration.java
+++ spring-whale-rbac/src/main/java/io/github/springwhale/rbac/SpringWhaleRBACConfiguration.java
@@ -1,12 +1,8 @@
 package io.github.springwhale.rbac;
 
-import io.github.springwhale.framework.webmvc.advice.SpringWhaleWebMvcResponseBodyAdvice;
-import io.github.springwhale.framework.webmvc.exception.SpringWhaleWebMvcExceptionHandler;
-import io.github.springwhale.framework.webmvc.exception.SpringWhaleWebMvcExceptionProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
 @ComponentScan
